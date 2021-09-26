440d9e80e5278bbabcbb691c87372d50c9d27468
Frank Liang <liangfeng610@gmail.com>
2021-09-26 22:25:58 +1300

    fix jwt auth backend

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,11 @@ const Viewer = require('../models/Viewer');
 
 const authMiddleware = async (req, res, next) => {
   let token;
-
-  if (req.headers.authentication) {
-    token = req.headers.authentication;
+  console.log(`req`, req.headers);
+  if (req.headers.authorization) {
+    token = req.headers.authorization;
   }
-
+  console.log(`token`, token);
   if (!token) {
     return next(
       res.status(401).json({
@@ -19,16 +19,22 @@ const authMiddleware = async (req, res, next) => {
 
   try {
     //Verify token
+    // Leave the console.log for testing purpose
+    console.log(`process.env.JWT_SECRET`, process.env.JWT_SECRET);
+
     const secret = Buffer.from(process.env.JWT_SECRET, 'base64');
+
+    console.log(`secret`, secret);
 
     const decode = jwt.verify(token, secret);
 
     console.log(`decode`, decode);
 
-    req.user = await Viewer.findById(decode.user_id);
-
+    req.user = await Viewer.findOne({ viewerId: decode.user_id });
+    console.log(`req.user`, req.user);
     next();
   } catch (error) {
+    console.log(`error`, error);
     return next(
       res.status(401).json({
         success: false,
