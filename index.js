4124bf3bc8e98a13fd7cebc39c1582ce2c61863f
Luke Percy <lpercyagile@gmail.com>
2021-10-01 19:53:43 +1300

    testing cors orgins

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.use(express.urlencoded());
 // Bypass the CORS error
 app.use(
   cors({
-    origin: ['https://42xd9tib4hce93bavmhmseapyp7fwj.ext-twitch.tv','http://localhost:3000'],
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+    origin:false,
+    methods: ['GET','HEAD','PUT','POST']
   })
 );
 
