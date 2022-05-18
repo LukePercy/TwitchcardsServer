011ef6cc30343e8c49ad059a03dbc729b505e2fe
Luke Percy <lpercyagile@gmail.com>
2022-05-18 21:32:19 +1200

    corrected clientID

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ app.use('/api', OAuthTwitch);
 
 //comfy
 const channel = process.env.TWITCH_USER;
-const clientId = process.env.CLIENT_ID;
+const clientId = process.env.TWITCH_CLIENT_ID;
 // Get the Channel Id from .env for now
 const channelId = process.env.CHANNEL_ID;
 let TwitchOAuthAccessToken;
