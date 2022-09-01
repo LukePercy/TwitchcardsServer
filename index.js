c97c90303fb794518501402574937f15128d7e5f
Luke Percy <lpercyagile@gmail.com>
2022-09-01 20:04:56 +1200

    added findorcreate to viewer.js to fix new user bug

--- index.js
+++ index.js
@@ -276,7 +276,7 @@ ComfyJS.onReward = async (user, reward, cost, message, extra) => {
               },
             ],
           });
-          const response = await responseFromCreateNewViewer.json();
+          const response = await responseFromCreateNewViewer.json(Viewer);
           // TODO: Need to test this part locally
           // Then get the newly created viewer's _id
           const dbchannelId = await getChannel(CHANNEL_ID);
